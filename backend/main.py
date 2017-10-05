542f35280e25f411ef3c6cd8dc106c5d8f96e76b
Omar Abou Mrad <omar.aboumrad@gmail.com>
2017-10-05 15:02:41 +0300

    passed dummy channel to tasks

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 import time
+import uuid
 
 import redis
 import rq
@@ -9,8 +10,8 @@ import tasks
 if __name__ == '__main__':
     connection = redis.Redis('redis', 6379)
     queue = rq.Queue(connection=connection)
-    job = queue.enqueue(tasks.run_django)
+    job = queue.enqueue(tasks.run_django, uuid.uuid4().hex)
 
     time.sleep(5)
 
-    print(job.result)
+    print(job.result.decode('utf-8'))
--- backend/tasks.py
+++ backend/tasks.py
@@ -1,6 +1,18 @@
 import docker
 
-client = docker.DockerClient(base_url='unix://var/run/docker.sock')
+from rq import get_current_job
 
-def run_django():
+
+def run_django(channel):
+
+    # channel will eventually be the actual reply_channel
+    # and instead of storing it in the meta, the result
+    # will be published to redis through PUBLISH and
+    # read back from the backend using SUBSCRIBE
+
+    job = get_current_job()
+    job.meta['channel'] = channel
+    job.save_meta()
+
+    client = docker.DockerClient(base_url='unix://var/run/docker.sock')
     return client.containers.run("djanground/executor", remove=True)
