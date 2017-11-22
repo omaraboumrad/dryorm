5bf3953433688acccabcb42aa4524ccdda0e3197
Omar Abou Mrad <omar.aboumrad@gmail.com>
2017-11-22 16:38:07 +0200

    added app server

--- backend/djanground/settings.py
+++ backend/djanground/settings.py
@@ -122,8 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
+    os.path.join(BASE_DIR, 'djanground', 'static')
 ]
 
-SNIPS_DIR = os.path.join(BASE_DIR, 'snips')
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
