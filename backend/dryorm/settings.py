c2e23f25a8986659d92dfe0f30299bded9944a22
Omar Abou Mrad <omar.aboumrad@gmail.com>
2017-11-24 16:50:23 +0200

    removed old javascript file

--- backend/dryorm/settings.py
+++ backend/dryorm/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '_807jt01(6^@-37g$86tjrv!l#crcf9#*clp-3*k0ym*sx3f@7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
